e91035238437759be9d8370cbdea18e3d7095284
Kyryl Step <skiril2000@ukr.net>
2020-10-17 08:44:21 +0300

    try create tesrt

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,11 +1,2 @@
-def even_or_odd(number):
-    num = int(input("Enter a number: "))
-    if ((num) % 2) == 0:
-        print ("{0} is Even".format(num))
-    else:
-        print ("{0} is Odd".format(num))
-
-
-print (even_or_odd(2))
-print (even_or_odd(0))
-print (even_or_odd(-1))
+def solution(s):
+    pass
